035a7d66a1c76098e4277d29b4ed5fdc9b15bd2b
dennystefani <dennystefani@gmail.com>
2019-10-22 17:09:49 +0200

    Alcune modifiche di prova molto interessanti

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.myfirstapp;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private static final String TAG = "MainActivity";
+    private TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        TextView numeri = findViewById(R.id.numeri);
+        Button startButton = findViewById(R.id.startButton);
+        Button stopButton = findViewById(R.id.stopButton);
+
+        startButton.setOnClickListener(v ->
+        {
+            textView.setText("Ho premuto start");
+            numeri.setText("");
+            for (Integer i = 0; i < 11; i++)
+            {
+
+                numeri.setText(numeri.getText() + " " + i.toString());
+            }
+        });
+        stopButton.setOnClickListener(v ->
+        {
+            textView.setText("Ho premuto stop");
+            numeri.setText("Ho stoppato");
+        });
     }
 }
