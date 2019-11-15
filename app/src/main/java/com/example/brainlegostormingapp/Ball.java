a557dd70a5ef93ab3fd2130271b7d1963420693d
dennystefani <dennystefani@gmail.com>
2019-11-15 22:06:31 +0100

    Aggiunte classi BallFinder e Ball

---
+++ app/src/main/java/com/example/brainlegostormingapp/Ball.java
@@ -0,0 +1,17 @@
+package com.example.brainlegostormingapp;
+
+import org.opencv.core.Point;
+
+public class Ball
+{
+    public Point center;
+    public float radius;
+    public String color;
+
+    public Ball(Point center, float radius, String color)
+    {
+        this.center = center;
+        this.radius = radius;
+        this.color = color;
+    }
+}
