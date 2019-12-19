f1823b23a1011178af4cac2194f5625f12951f30
Marco Negri <skizz98@gmail.com>
2019-12-19 21:44:46 +0100

    Alcuni commenti + modifiche del cazzo

--- app/src/main/java/com/example/brainlegostormingapp/Activity/AutoActivity.java
+++ app/src/main/java/com/example/brainlegostormingapp/Activity/AutoActivity.java
@@ -218,22 +218,27 @@ public class AutoActivity extends AppCompatActivity implements MyRecyclerViewAda
         });
 
 
+        //todo la matrice viene inizializzata qui. ma poi andrebbe aggiornata
+        // (oppure inizializata in un onlick da qualche altra parte)
+        //data to populate the RecyclerView with
+        String[] data = {"0","0","0","MINA","0","0","0","ROBOT","0",};
 
-        // data to populate the RecyclerView with
-        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
-
-        // set up the RecyclerView
+        //TODO sto codice va spostato ma intanto teniamolo qui finche ci lavoriamo sopra
+        //set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.matrixField);
-        int numberOfColumns = 6;
+        //numberOfColumns andrebbe cambiato con il valore di colonne della matrice
+        int numberOfColumns = 3;
         recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
         adapter = new MyRecyclerViewAdapter(this, data);
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
     }
 
+
+    //TODO: qui va messa la logica che si vuole implementare per un click sulla matrice
     @Override
     public void onItemClick(View view, int position) {
-        Log.i("TAG", "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position);
+        Log.i("TAG", "You clicked " + adapter.getItem(position) + ", which is at cell position " + position);
     }
 
     //termina la prova, calcola e mostra il tempo finale e TODO mostra la matrice
